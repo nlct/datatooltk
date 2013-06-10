42531b190b6fd29b4072c3e97bc37da5a421bda2
nlct <n.talbot@uea.ac.uk>
2013-06-10 10:30:22 +0100

    Restructured DatatoolDbPanel

--- src/java/gui/DatatoolDbPanel.java
+++ src/java/gui/DatatoolDbPanel.java
@@ -1,17 +1,18 @@
 package com.dickimawbooks.datatooltk.gui;
 
 import java.io.*;
+import java.awt.BorderLayout;
 import javax.swing.*;
 import javax.swing.table.*;
 
 import com.dickimawbooks.datatooltk.DatatoolDb;
 import com.dickimawbooks.datatooltk.DatatoolTk;
 
-public class DatatoolDbPanel extends JScrollPane
+public class DatatoolDbPanel extends JPanel
 {
    public DatatoolDbPanel(DatatoolDb db)
    {
-      super();
+      super(new BorderLayout());
 
       this.db = db;
       setName(db.getName());
@@ -21,60 +22,14 @@ public class DatatoolDbPanel extends JScrollPane
 
    private void initTable()
    {
-      JTable table = new JTable(new AbstractTableModel()
-      {
-         public String getColumnName(int col)
-         {
-            return db.getHeader(col+1).getTitle();
-         }
-
-         public int getRowCount()
-         {
-            return db.getRowCount();
-         }
-
-         public int getColumnCount()
-         {
-            return db.getColumnCount();
-         }
-
-         public Class<?> getColumnClass(int column)
-         {
-            switch (db.getHeader(column+1).getType())
-            {
-               case DatatoolDb.TYPE_INTEGER:
-                  return Integer.class;
-               case DatatoolDb.TYPE_REAL:
-                  return Float.class;
-            }
-
-            return String.class;
-         }
-
-         public Object getValueAt(int row, int col)
-         {
-            return db.getValue(row+1, col+1);
-         }
-
-         public void setValueAt(Object value, int row, int col)
-         {
-            db.getRow(row+1).setCell(col+1, value.toString());
-            isModified = true;
-            fireTableCellUpdated(row, col);
-         }
-
-         public boolean isCellEditable(int row, int column)
-         {
-            return true;
-         }
-      });
+      JTable table = new JTable(new DatatoolDbTableModel(db, this));
 
       table.setRowHeight(100);
       table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
       table.setDefaultRenderer(String.class, new DbCellRenderer());
       table.setDefaultEditor(String.class, new DbCellEditor());
 
-      setViewportView(table);
+      add(new JScrollPane(table), BorderLayout.CENTER);
    }
 
    public void save(String filename)
@@ -120,6 +75,11 @@ public class DatatoolDbPanel extends JScrollPane
       return isModified;
    }
 
+   public void setModified(boolean modified)
+   {
+      isModified = modified;
+   }
+
    public String getToolTipText()
    {
       File file = db.getFile();
@@ -132,3 +92,60 @@ public class DatatoolDbPanel extends JScrollPane
    private boolean isModified = false;
 }
 
+class DatatoolDbTableModel extends AbstractTableModel
+{
+   private DatatoolDb db;
+   private DatatoolDbPanel panel;
+
+   public DatatoolDbTableModel(DatatoolDb db, DatatoolDbPanel panel)
+   {
+      super();
+      this.db = db;
+      this.panel = panel;
+   }
+
+   public String getColumnName(int col)
+   {
+      return db.getHeader(col+1).getTitle();
+   }
+
+   public int getRowCount()
+   {
+      return db.getRowCount();
+   }
+
+   public int getColumnCount()
+   {
+      return db.getColumnCount();
+   }
+
+   public Class<?> getColumnClass(int column)
+   {
+      switch (db.getHeader(column+1).getType())
+      {
+         case DatatoolDb.TYPE_INTEGER:
+            return Integer.class;
+         case DatatoolDb.TYPE_REAL:
+            return Float.class;
+      }
+
+      return String.class;
+   }
+
+   public Object getValueAt(int row, int col)
+   {
+      return db.getValue(row+1, col+1);
+   }
+
+   public void setValueAt(Object value, int row, int col)
+   {
+      db.getRow(row+1).setCell(col+1, value.toString());
+      panel.setModified(true);
+      fireTableCellUpdated(row, col);
+   }
+
+   public boolean isCellEditable(int row, int column)
+   {
+      return true;
+   }
+}
