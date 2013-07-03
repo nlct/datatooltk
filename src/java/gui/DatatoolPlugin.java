ff8971ba32b8d2cda31a47ee44712e450a5e8e03
nlct <n.talbot@uea.ac.uk>
2013-07-03 11:20:18 +0100

    Refactored plugin code

--- src/java/gui/DatatoolPlugin.java
+++ src/java/gui/DatatoolPlugin.java
@@ -52,30 +52,34 @@ public class DatatoolPlugin implements Runnable
       pb.redirectErrorStream(true);
 
       BufferedReader reader = null;
-      BufferedWriter writer = null;
+      PrintWriter writer = null;
 
       try
       {
          Process process = pb.start();
 
+         writer = new PrintWriter(process.getOutputStream());
+
+         writer.println(dbPanel.getRowCount());
+         writer.println(dbPanel.getColumnCount());
+         writer.println(dbPanel.getSelectedRow());
+         writer.println(dbPanel.getSelectedColumn());
+
+         writer.println("--EOF--");
+
+         writer.close();
+
          reader = new BufferedReader(
             new InputStreamReader(process.getInputStream()));
 
-         writer = new BufferedWriter(
-            new OutputStreamWriter(process.getOutputStream()));
 
          String line;
 
          while ((line = reader.readLine()) != null)
          {
-            if (!processLine(line, writer))
-            {
-               break;
-            }
          }
 
          reader.close();
-         writer.close();
 
          int exitCode = process.waitFor();
 
@@ -93,80 +97,10 @@ public class DatatoolPlugin implements Runnable
 
    }
 
-   private boolean processLine(String line, BufferedWriter writer)
-     throws IOException
-   {
-      Matcher matcher = PATTERN_PLUGIN.matcher(line);
-
-      if (matcher.matches())
-      {
-         String statement = matcher.group(1);
-
-         if (statement.equals("QUERY"))
-         {
-            queryStatement(matcher.group(2), writer);
-         }
-         else
-         {
-            return commandStatement(matcher.group(2), writer);
-         }
-      }
-      else
-      {
-         throw new IOException(DatatoolTk.getLabelWithValue(
-            "error.plugin.unknown_message", line));
-      }
-
-      return true;
-   }
-
-   private boolean commandStatement(String command, BufferedWriter writer)
-     throws IOException
-   {
-      if (command.equals("EXIT"))
-      {
-         return false;
-      }
-      else
-      {
-         throw new IOException(DatatoolTk.getLabelWithValue(
-            "error.plugin.unknown_command", command));
-      }
-   }
-
-   private void queryStatement(String query, BufferedWriter writer)
-   throws IOException
-   {
-      if (query.equals("ROW COUNT"))
-      {
-         writer.write(dbPanel.getRowCount()+"\n");
-      }
-      else if (query.equals("COLUMN COUNT"))
-      {
-         writer.write(dbPanel.getColumnCount()+"\n");
-      }
-      else if (query.equals("SELECTED ROW"))
-      {
-         writer.write(dbPanel.getSelectedRow()+"\n");
-      }
-      else if (query.equals("SELECTED COLUMN"))
-      {
-         writer.write(dbPanel.getSelectedColumn()+"\n");
-      }
-      else
-      {
-         throw new IOException(DatatoolTk.getLabelWithValue(
-            "error.plugin.unknown_query", query));
-      }
-
-      writer.flush();
-   }
 
    private File pluginFile;
    private String name, perl;
 
    private DatatoolDbPanel dbPanel;
 
-   private static final Pattern PATTERN_PLUGIN 
-      = Pattern.compile("PLUGIN (QUERY|COMMAND) ([\\w\\s]+):>>");
 }
