dfc16114cf8a3682f967a91924bce49a71b8888e
nlct <n.talbot@uea.ac.uk>
2013-07-09 13:11:26 +0100

    Version 0.4b

--- src/java/DatatoolTk.java
+++ src/java/DatatoolTk.java
@@ -1113,9 +1113,9 @@ public class DatatoolTk
 
    private static boolean guiMode = false;
 
-   public static final String appVersion = "0.3b";
+   public static final String appVersion = "0.4b";
    public static final String appName = "datatooltk";
-   public static final String appDate = "2013-07-04";
+   public static final String appDate = "2013-07-09";
 
    private static Properties dictionary;
    private static boolean debugMode = false;
