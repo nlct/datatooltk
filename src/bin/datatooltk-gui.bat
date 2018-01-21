5003eafc435cd2ea50f2f4160e71a4eb9df82395
nlct <n.talbot@uea.ac.uk>
2018-01-21 14:54:25 +0000

    added -Djava.locale.providers=CLDR,JRE

--- src/bin/datatooltk-gui.bat
+++ src/bin/datatooltk-gui.bat
@@ -1,1 +1,1 @@
-@start javaw -splash:"%~dp0\..\lib\datatooltk-splash.png" -jar "%~dp0\..\lib\datatooltk.jar" --gui %*
+@start javaw -Djava.locale.providers=CLDR,JRE -splash:"%~dp0\..\lib\datatooltk-splash.png" -jar "%~dp0\..\lib\datatooltk.jar" --gui %*
